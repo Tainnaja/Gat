ee2c340fb002fe3233afa1b37216ae868a209107
wanrat40 <namo_krub@hotmail.com>
2020-01-16 17:34:36 +0700

    จัดไฟล์

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 
-@extends('layouts.main')
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -11,19 +10,18 @@
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/icon" href="{{ URL::asset('image/favicon.ico') }}">
     <!-- Font Awesome -->
-    <link href="https://fonts.googleapis.com/css?family=Prompt&display=swap" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
     <!-- Bootstrap -->
-    <link href="{{ URL::asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('first_page/css/bootstrap.min.css') }}" rel="stylesheet">
     <!-- Slick slider -->
-    <link href="{{ URL::asset('css/slick.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('first_page/css/slick.css') }}" rel="stylesheet">
     <!-- Theme color -->
-    <link id="switcher" href="{{ URL::asset('css/theme-color/default-theme.css') }}" rel="stylesheet">
+    <link id="switcher" href="{{ URL::asset('first_page/css/theme-color/default-theme.css') }}" rel="stylesheet">
 
     <!-- Main Style -->
-    <link href="{{ URL::asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('first_page/css/style.css') }}" rel="stylesheet">
 
     <!-- Fonts -->
-	<link href="https://fonts.googleapis.com/css?family=Prompt&display=swap" rel="stylesheet">
 
     <!-- Open Sans for body font -->
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700,800" rel="stylesheet">
@@ -38,11 +36,50 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-  </head>
+</head>
 
   <body> 	   
   	
-	
+	<!-- Start Header -->
+	<header id="mu-header" class="" role="banner">
+		<div class="container">
+			<nav class="navbar navbar-default mu-navbar">
+			  	<div class="container-fluid">
+				    <!-- Brand and toggle get grouped for better mobile display -->
+				    <div class="navbar-header">
+				      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				        <span class="sr-only">Toggle navigation</span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				      </button>
+
+				      <!-- Text Logo -->
+				      <a class="navbar-brand" href="index.html"><i class="fa fa-book" aria-hidden="true"></i> GatOnline</a>
+
+				      <!-- Image Logo -->
+				      <!-- <a class="navbar-brand" href="index.html"><img src="assets/images/logo.png"></a> -->
+
+
+				    </div>
+
+				    <!-- Collect the nav links, forms, and other content for toggling -->
+				    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				      	<ul class="nav navbar-nav mu-menu navbar-right">
+					        <li><a href="">HOME</a></li>					        
+					        <li><a href="">ACHIEVEMENT</a></li>
+				            <li><a href="">YOUR SKILL</a></li>
+				            <li><a href="">QUESTIONNARIE</a></li>
+							<li><a href="">ACCOUNT SETTING</a></li>				            
+							<li><a href="">LOG IN</a></li>
+							<li><a href="">SIGN IN</a></li>
+				      	</ul>
+				    </div><!-- /.navbar-collapse -->
+			  	</div><!-- /.container-fluid -->
+			</nav>
+		</div>
+	</header>
+	<!-- End Header -->
 
 	<!-- Start Featured Slider -->
 
@@ -362,18 +399,18 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <!-- Bootstrap -->
-    <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ URL::asset('first_page/js/bootstrap.min.js') }}"></script>
 	<!-- Slick slider -->
-    <script type="text/javascript" src="{{ URL::asset('js/slick.min.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('first_page/js/slick.min.js') }}"></script>
     <!-- Counter js -->
-    <script type="text/javascript" src="{{ URL::asset('js/counter.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('first_page/js/counter.js') }}"></script>
     <!-- Ajax contact form  -->
-    <script type="text/javascript" src="{{ URL::asset('js/app.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('first_page/js/app.js') }}"></script>
    
  
 	
     <!-- Custom js -->
-	<script type="text/javascript" src="{{ URL::asset('js/custom.js') }}"></script>
+	<script type="text/javascript" src="{{ URL::asset('first_page/js/custom.js') }}"></script>
 	
     
   </body>
