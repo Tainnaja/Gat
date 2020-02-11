6e893239f7c29cce137da1d29e8300e278f8da20
dew com <diwinw01@gmail.com>
2020-02-11 16:21:12 +0700

    login

--- app/Http/Controllers/GatController.php
+++ app/Http/Controllers/GatController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class GatController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth', ['only' =>
+        ['fulltest']]);
+     }
     public function index() {  
         return view('Gat.index');
     }
