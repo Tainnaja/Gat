3657558e4467864eb72aaa93ded14a926b1d58cf
dew com <diwinw01@gmail.com>
2020-02-13 19:43:53 +0700

    แก้ admin เข้าไม่ได้

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -3,15 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class ExamController extends Controller
 {
+
+    public function __construct(){
+        $this->middleware('auth', ['only' =>
+        ['admin','createTest']]);
+     }
     // admin
     public function admin() {  
-        return view('Gat.admin');
+        try   
+        {
+            if (Auth::user()->role_id===2) 
+            {
+                return view('Exam.admin');
+            }
+            else
+            {
+               return view('Gat.index');
+            }
+        
+         } catch (Exception $e) {
+            return view('Gat.index');
+        }
+        
     }
 
     public function createTest() {  
-        return view('Gat.createTest');
+        try   
+        {
+            if (Auth::user()->role_id===2) 
+            {
+                return view('Exam.createTest');
+            }
+            else
+            {
+               return view('Gat.index');
+            }
+        
+         } catch (Exception $e) {
+            return view('Gat.index');
+        }
+        
     }
 }
